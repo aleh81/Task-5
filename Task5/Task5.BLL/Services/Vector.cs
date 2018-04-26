f925e254b250b8728cd30ef825bf35f3d0933a6b
Oleg <User@DESKTOP-2L28T8V>
2018-04-26 14:40:02 +0300

    Creating class Vector

---
+++ Task5/Task5.BLL/Services/Vector.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5.BLL.Services
+{
+	/// <summary>
+	/// Class describing a vector
+	/// </summary>
+	public class Vector
+	{
+		/// <summary>
+		/// / West - east direction
+		/// </summary>
+		public double X { get; set; }
+		/// <summary>
+		/// Up and down direction
+		/// </summary>
+		public double Y { get; set; }
+		/// <summary>
+		/// North-south direction
+		/// </summary>
+		public double Z { get; set; }
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public Vector(): this(0, 0, 0)
+		{
+		}
+
+		/// <summary>
+		/// Constructor initializing vector
+		/// </summary>
+		/// <param name="x"></param>
+		/// <param name="y"></param>
+		/// <param name="z"></param>
+		public Vector(double x, double y, double z)
+		{
+			X = x;
+			Y = y;
+			Z = z;
+		}
+	}
+}
